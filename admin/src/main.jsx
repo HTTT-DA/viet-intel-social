fd5a3dd09786044a5f2964250ecfa70ce1f361df
Tristan Anh Ngo <54490272+tristanhuyanhngo@users.noreply.github.com>
2023-07-17 15:59:47 +0700

    refactor: FE - Refactor code for Admin
    Done

---
+++ admin/src/main.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.css";
+import Login from "./pages/login/index.jsx";
+import Dashboard from "./pages/dashboard";
+import NotFound from "./pages/notFound";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    children: [
+      {
+        path: "/",
+        element: <Dashboard />,
+      },
+    ],
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  // 404 not found
+  {
+    path: "*",
+    element: <NotFound />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
