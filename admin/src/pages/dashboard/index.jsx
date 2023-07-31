0ce3a4a91b5c8eeea2247c86433d1fb1e9b16112
Tristan Anh Ngo <54490272+tristanhuyanhngo@users.noreply.github.com>
2023-07-31 14:41:29 +0700

    fix: FE Admin Login
    Comment some line of codes

--- admin/src/pages/dashboard/index.jsx
+++ admin/src/pages/dashboard/index.jsx
@@ -2,18 +2,18 @@ import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 function Dashboard() {
-  const navigate = useNavigate();
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    if (
-      !window.localStorage.getItem("userId") ||
-      !window.localStorage.getItem("email") ||
-      !window.localStorage.getItem("accessToken")
-    ) {
-      console.log("redirecting");
-      navigate("/login");
-    }
-  }, [navigate]);
+  // useEffect(() => {
+  //   if (
+  //     !window.localStorage.getItem("userId") ||
+  //     !window.localStorage.getItem("email") ||
+  //     !window.localStorage.getItem("accessToken")
+  //   ) {
+  //     console.log("redirecting");
+  //     navigate("/login");
+  //   }
+  // }, [navigate]);
 
   return <div>Dashboard</div>;
 }
